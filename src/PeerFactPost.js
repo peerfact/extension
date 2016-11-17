14b73bf7bf1446852dee32742fd9f9ddd17b83c8
Sam MacPherson <xylom44@gmail.com>
2016-11-17 17:58:32 -0500

    version 0.4.0; added satirical websites

--- src/PeerFactPost.js
+++ src/PeerFactPost.js
@@ -34,6 +34,8 @@ PeerFactPost.prototype.refresh = function () {
  * Perform a check to see if this post is fake news or not.
  */
 PeerFactPost.prototype.checkPost = function () {
+	var self = this;
+
 	var anchors = this.$root.find(this.manifest.linkSelector);
 	for (var i = 0; i < anchors.length; i++) {
 		var url = new URL($(anchors.get(i)).attr("href"));
@@ -42,11 +44,22 @@ PeerFactPost.prototype.checkPost = function () {
 			if (checkHost(url.host, "facebook.com") && url.searchParams.has("u")) {
 				url = new URL(url.searchParams.get("u"));
 			}
-			for (var o = 0; o < this.manifest.fakenews.length; o++) {
-				var apex = this.manifest.fakenews[o].host;
-				if (checkHost(url.host, apex)) {
-					//Found a fake news source.. style this
-					return { apex:apex, type:"fakenews" };
+			function checkType (type) {
+				for (var o = 0; o < self.manifest[type].length; o++) {
+					var apex = self.manifest[type][o].host;
+					if (checkHost(url.host, apex)) {
+						//Found a fake news source.. style this
+						return { apex:apex, type:type };
+					}
+				}
+
+				return null;
+			}
+			var typesToCheck = ["fakenews", "satire"];
+			for (var o = 0; o < typesToCheck.length; o++) {
+				var result = checkType(typesToCheck[o]);
+				if (result != null) {
+					return result;
 				}
 			}
 		}
@@ -66,20 +79,30 @@ PeerFactPost.prototype.markVisited = function () {
 
 PeerFactPost.prototype.styleIt = function (info, domainInfo) {
 	var self = this;
+	var boxText = "";
+	var commentText = "";
+	var detailsLink = "https://www.peerfact.xyz/details.html?domain=" + encodeURIComponent(info.apex);
 
 	this.$root.addClass("peerfact-" + info.type);
 
-	var detailsLink = "https://www.peerfact.xyz/details.html?domain=" + encodeURIComponent(info.apex);
-	this.$insertionPoint.prepend("<div class='peerfact-box'><div class='peerfact-warning'>WARNING! " + domainInfo.title + " is a known fake news site.</div><div><a class='peerfact-link-button' href='" + detailsLink + "' target='_blank'>Details</a><a class='peerfact-link-button peerfact-do-comment' href='#'>Comment this to Others</a></div></div>");
+	switch (info.type) {
+		case "fakenews":
+			boxText = "WARNING! " + domainInfo.title + " is a known fake news site.";
+			commentText = domainInfo.title + " is a known fake news site. See " + detailsLink + " for details.";
+		case "satire":
+			boxText = "Heads up! " + domainInfo.title + " is satire. Don't take it literally. :)";
+			commentText = domainInfo.title + " is satire. See " + detailsLink + " for details.";
+	}
+
+	this.$insertionPoint.prepend("<div class='peerfact-box'><div class='peerfact-description'>" + boxText + "</div><div><a class='peerfact-link-button' href='" + detailsLink + "' target='_blank'>Details</a><a class='peerfact-link-button peerfact-do-comment' href='#'>Comment this to Others</a></div></div>");
 	this.$insertionPoint.find(".peerfact-do-comment").click(function (e) {
 		e.preventDefault();
 
-		var text = domainInfo.title + " is a known fake news site. See " + detailsLink + " for details.";
 		var postId = self.getPostId();
 		if (postId) {
-			PeerFactCommunicator.send("facebook", "comment", { postid:postId, text:text });
+			PeerFactCommunicator.send("facebook", "comment", { postid:postId, text:commentText });
 		} else {
-			self.$insertionPoint.find(".peerfact-box").append("<div class='peerfact-error'>An error occurred. Sorry we couldn't post for you. You can copy + paste this manually if you like: <strong>" + text + "</strong></div>");
+			self.$insertionPoint.find(".peerfact-box").append("<div class='peerfact-error'>An error occurred. Sorry we couldn't post for you. You can copy + paste this manually if you like: <strong>" + commentText + "</strong></div>");
 		}
 	});
 };
